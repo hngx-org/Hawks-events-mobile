a6ac35ba8e07201ed665bfc26501ff5b83b1b146
ozo-vehe <johnnie.vehe@gmail.com>
2023-09-20 20:43:20 +0100

    finished group details, edited event component

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -15,7 +15,7 @@ const AppNavigator = () => {
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Login' }} />
       <Stack.Screen name="Main" component={MainNavigator} options={{ headerShown: false }} />
-        <Stack.Screen name="Group Details" component={GroupDetailsScreen} options={{ title: 'Group Details' }} />
+        <Stack.Screen name="Group Details" component={GroupDetailsScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Create Group" component={CreateGroupScreen} options={{ title: 'Create Group' }} />
         <Stack.Screen name="Event Details" component={EventDetailsScreen} options={{ title: 'Event Details' }} />
         <Stack.Screen name="Create Event" component={CreateEventScreen} options={{ title: 'Create Event' }} />
