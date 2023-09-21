4108031e824a17b16cee6e7edd096697754f631a
Daniel Adekoya <adekoyadaniel53@gmail.com>
2023-09-21 12:46:35 +0100

    chore: create event  screen

---
+++ app/components/shared/AppButton.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+import {phoneHeight} from '../../utils/dimensions';
+
+export default function AppButton({
+  title = 'Click Me',
+  onPress,
+  bgColor = '#FF9405',
+  color = '#FFFFFF',
+  disabled,
+  style,
+}) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        width: '100%',
+        backgroundColor: bgColor,
+        borderRadius: 10,
+        marginBottom: 10,
+        marginTop: 20,
+        ...style,
+      }}>
+      <Text
+        disabled={disabled}
+        style={{
+          textTransform: 'capitalize',
+          textAlign: 'center',
+          paddingVertical: (phoneHeight * 0.03) / 2,
+          fontSize: 18,
+          borderColor: bgColor,
+          fontFamily: 'Satoshi-Black',
+          color: '#FFFFFF',
+          fontWeight: 800,
+          ...style,
+        }}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
--- app/screens/CreateEventScreen.js
+++ app/screens/CreateEventScreen.js
@@ -1,30 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import AppInput from '../components/shared/AppInput';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { Keyboard } from 'react-native';
 import DateSelect from '../components/DateSelect';
+import { phoneHeight } from '../utils/dimensions';
+import AppSelect from '../components/shared/AppSelect';
+import AppButton from '../components/shared/AppButton';
 
 
 const CreateEventScreen = ({ navigation }) => {
- 
+  const [value, setValue] = useState(null);
+  const [options, setOptions] = useState([
+    { label: "Male", value: "male" },
+    { label: "Female", value: "female" },
+    { label: "Prefer Not to Say", value: "neutral" },
+  ]);
   return (
+    <ScrollView>
     <View style={styles.container}>
+    <View style={styles.container2}>
           <AppInput label='Event Name' />
           <AppInput label='Description' multiline={true} numberOfLines={3}/>
-<DateSelect label='Start Date'/>
+          <DateSelect label='Start Date'/>
           <AppInput label='Location' />
+          <AppSelect label='Select Group' options={options} value={value} setOptions={setOptions} setValue={setValue}/>
+          <AppButton title='Create' />
     </View>
+   {/*  <View style={{
+    paddingHorizontal: 20,
+    width: '100%'
+    }}>
+    </View> */}
+
+    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent:'center', 
+    alignItems: 'center',  
+    backgroundColor: 'white',
+    width: '100%',
+    height: phoneHeight/2*1.81
+  },
+  container2: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     paddingHorizontal: 20,
-    backgroundColor: 'white'
+  width: '100%'
   },
   image: {
     width: 200, // Adjust the width as needed
