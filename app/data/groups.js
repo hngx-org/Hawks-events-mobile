dffa5b2ed9854a5999e63c219850dcfcfa66070e
ozo-vehe <johnnie.vehe@gmail.com>
2023-09-22 09:42:15 +0100

    Edited the groupComponent, fixed component errors

---
+++ app/data/groups.js
@@ -0,0 +1,99 @@
+/* eslint-disable prettier/prettier */
+export const groups = [
+  {
+    groupId: 0,
+    name: 'GGB Dance',
+    members: 100,
+    events: [0, 1, 2, 3, 4],
+  },
+  {
+    groupId: 1,
+    name: 'GGB Party',
+    members: 100,
+    events: [5, 6, 7],
+  },
+  {
+    groupId: 2,
+    name: 'GGB Harvest',
+    members: 100,
+    events: [8, 9, 10, 11],
+  },
+  {
+    groupId: 3,
+    name: 'GGB Thanksgiving',
+    members: 100,
+    events: [12, 13],
+  },
+  {
+    groupId: 4,
+    name: 'GGB Dance',
+    members: 100,
+    events: [14, 15, 16, 17, 18],
+  },
+  {
+    groupId: 5,
+    name: 'GGB Party',
+    members: 100,
+    events: [19, 20, 21],
+  },
+  {
+    groupId: 6,
+    name: 'GGB Harvest',
+    members: 100,
+    events: [22, 23],
+  },
+  {
+    groupId: 7,
+    name: 'GGB Thanksgiving',
+    members: 100,
+    events: [5],
+  },
+  {
+    groupId: 8,
+    name: 'GGB Dance',
+    members: 100,
+    events: [0, 1, 2],
+  },
+  {
+    groupId: 9,
+    name: 'GGB Party',
+    members: 100,
+    events: [3, 4],
+  },
+  {
+    groupId: 10,
+    name: 'GGB Harvest',
+    members: 100,
+    events: [10, 13, 14, 15],
+  },
+  {
+    groupId: 11,
+    name: 'GGB Thanksgiving',
+    members: 100,
+    events: [12, 16, 17],
+  },
+  {
+    groupId: 12,
+    name: 'GGB Dance',
+    members: 100,
+    events: [6, 7, 8, 9],
+  },
+  {
+    groupId: 13,
+    name: 'GGB Party',
+    members: 100,
+    events: [18, 20, 23],
+  },
+  {
+    groupId: 14,
+    name: 'GGB Harvest',
+    members: 100,
+    events: [19, 21],
+  },
+  {
+    groupId: 15,
+    name: 'GGB Thanksgiving',
+    members: 100,
+    events: [22],
+  },
+];
